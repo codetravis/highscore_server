72c50ee7485c1fa7fc0ae0eb23b8cadb6125b859
Travis Cooper <tcooper@Traviss-MacBook-Pro.local>
2015-02-07 23:06:05 -0500

    adding web game to see if this CORS thing works

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 
+set :public_folder, File.dirname(__FILE__) + "/html5"
+
 get '/' do
-    response.headers['Access-Control-Allow-Origin'] = *
-    "High Score List"
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    body "High Score List"
 end
 
 post '/' do
