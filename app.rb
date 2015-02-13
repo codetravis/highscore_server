eaf479de99e0f7a036afeeb469b6137f8259d082
Travis Cooper <tcooper@Traviss-MacBook-Pro.local>
2015-02-13 17:23:04 -0500

    using a txt file as a simple highscore list

--- app.rb
+++ app.rb
@@ -4,9 +4,28 @@ set :public_folder, File.dirname(__FILE__) + "/html5"
 
 get '/' do
     response.headers['Access-Control-Allow-Origin'] = "*"
-    body "High Score List"
+    high_scores = File.read('highscores.txt')
+    body high_scores
 end
 
 post '/' do
-     "Do nothing"
+     score_list = []
+     score_file = File.open('highscores.txt', 'rw')
+     # parse score file into an array of hashes
+     score_file.each_line do |line|
+       line_fields = line.split(":")
+       score_list.push({initials: line_fields[0], score: line_fields[1]})
+     end
+     # parse data passed in from game into hash
+     player_score = data.split(":")
+     # see if score is higher than any of our stored scores
+     score_list.each do |high_score|
+       if player_score[1] > high_score[:score]
+         high_score[:initials] = player_score[0]
+	 high_score[:score] = player_score[1]
+	 break
+       end
+       # write new scores to file
+       score_file.write(high_score[:initials] + ":" + high_score[:score])       
+     end
 end
