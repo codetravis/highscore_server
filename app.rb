ba9e183857a9b6df32f542de3dfd0f871b3fd5a6
Travis Cooper <tcooper@Traviss-MacBook-Pro.local>
2015-02-14 22:50:15 -0500

    writing all scores back to file

--- app.rb
+++ app.rb
@@ -16,19 +16,20 @@ post '/:initials/:score' do
        line_fields = line.split(":")
        score_list.push({initials: line_fields[0], score: line_fields[1]})
      end
+     all_scores = ""
      # see if score is higher than any of our stored scores
      if score_list.length >= 10
        score_list.each do |high_score|
-         if player_score[1].to_i > high_score[:score].to_i
+         if params[:score].to_i > high_score[:score].to_i
            high_score[:initials] = params[:initials]
 	   high_score[:score] = params[:score]
-	   break
          end
          # write new scores to file
-         score_file.write(high_score[:initials] + ":" + high_score[:score])       
+         all_scores += high_score[:initials] + ":" + high_score[:score] + "\n"       
        end
      else
-       score_list.write(player_score[0] + ":" + player_score[1])
+       all_scores += player_score[0] + ":" + player_score[1] + "\n"
      end
+     score_file.write(all_scores)
      score_file.close
 end
